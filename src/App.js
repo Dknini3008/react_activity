bb91f2bf7c8cac0e9b70725304232cb9f46fc4ef
Brayan Decanini <a6519150009@utch.edu.mx>
2021-05-27 10:29:16 -0600

    Making a dynamic Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Layout from "./pages/Layout/Layout"
 import Main from "./pages/main/Main"
+import Badge from "./components/Badge"
 
 
 function App() {
   return (
     <React.Fragment>
       <Layout>
-        <Main title={"Welcome"}></Main>
+        <Badge 
+          profile_picture={"https://images.pexels.com/photos/1499327/pexels-photo-1499327.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        }
+        ></Badge>
       </Layout>
     </React.Fragment>
   );
